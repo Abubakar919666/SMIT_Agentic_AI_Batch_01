a60ac4d945f9e444d9dd5a8bca9549cd48d93352
Abubakar919666 <115037887+Abubakar919666@users.noreply.github.com>
2025-11-01 16:47:13 +0500

    path parameter

--- FastApi/main.py
+++ FastApi/main.py
@@ -1,15 +1,8 @@
-from typing import Union
-
 from fastapi import FastAPI
 
 app = FastAPI()
 
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    
+def message_hello():
+    return {'message': 'hello world has been excicuted'}
