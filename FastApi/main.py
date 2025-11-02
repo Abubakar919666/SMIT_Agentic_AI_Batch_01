67724690e86e6266401edeef1fb90cdb0a38bbaf
Abubakar919666 <115037887+Abubakar919666@users.noreply.github.com>
2025-11-02 16:22:29 +0500

    update

--- FastApi/main.py
+++ FastApi/main.py
@@ -4,5 +4,9 @@ app = FastAPI()
 
 
 @app.get("/")
-def message_hello():
+def hello():
     return {'message': 'hello world has been excicuted'}
+
+@app.get("/about")
+def about():
+    return {"me"}
