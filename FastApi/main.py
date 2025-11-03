8b6bd4a17529b854f3fbc83687ec90a4c1e6a6eb
Abubakar919666 <115037887+Abubakar919666@users.noreply.github.com>
2025-11-03 10:33:36 +0500

    update json file

--- FastApi/main.py
+++ FastApi/main.py
@@ -1,12 +1,35 @@
 from fastapi import FastAPI
+import json
 
 app = FastAPI()
+
+def load_data():
+    with open('patient.json', 'r') as f:
+        data = json.load(f)
+
+    return(data)
 
 
 @app.get("/")
 def hello():
-    return {'message': 'hello world has been excicuted'}
+    return {"message": "Patient management system API"}
 
 @app.get("/about")
 def about():
-    return {"me"}
+    return {"message": "A fully functional API System"}
+
+@app.get("/view")
+def view():
+    data = load_data()
+
+    return(data)
+
+
+@app.get("/patient/{patient_id}")
+def view_patient(patient_id: str):
+ # load all the data 
+ data = load_data
+
+ if patient_id in data:
+   return data[patient_id]
+ return{"error" : "patient not found"}
