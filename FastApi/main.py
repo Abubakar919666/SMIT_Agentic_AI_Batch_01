b1e914bee13acf4ca37eb33f6b462054dc532a6c
Abubakar919666 <115037887+Abubakar919666@users.noreply.github.com>
2025-11-04 10:41:34 +0500

    error has been solved

--- FastApi/main.py
+++ FastApi/main.py
@@ -1,5 +1,5 @@
-from fastapi import FastAPI
 import json
+from fastapi import FastAPI, Path, HTTPException
 
 app = FastAPI()
 
@@ -12,24 +12,23 @@ def load_data():
 
 @app.get("/")
 def hello():
-    return {"message": "Patient management system API"}
+    return {'message':'Patient Management System API'}
 
-@app.get("/about")
+@app.get('/about')
 def about():
-    return {"message": "A fully functional API System"}
+    return {'message': 'A fully functional API to manage your patient records'}
 
-@app.get("/view")
+@app.get('/view')
 def view():
     data = load_data()
 
-    return(data)
+    return data
 
+@app.get('/patient/{patient_id}')
+def view_patient(patient_id: str = Path(..., description='ID of the patient in the DB', example='P001')):
+    # load all the patients
+    data = load_data()
 
-@app.get("/patient/{patient_id}")
-def view_patient(patient_id: str):
- # load all the data 
- data = load_data
-
- if patient_id in data:
-   return data[patient_id]
- return{"error" : "patient not found"}
+    if patient_id in data:
+        return data[patient_id]
+    raise HTTPException(status_code=404, detail='Patient not found')
